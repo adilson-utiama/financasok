4da221f416f2f6ccd8b540003c0780407432225a
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-10-01 21:55:16 -0300

    Adicionado preference desativar tela de abertura

--- app/src/main/java/com/asuprojects/walletok/ui/SplahScreenActivity.java
+++ app/src/main/java/com/asuprojects/walletok/ui/SplahScreenActivity.java
@@ -1,12 +1,11 @@
 package com.asuprojects.walletok.ui;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.asuprojects.walletok.MainActivity;
@@ -15,16 +14,19 @@ import com.asuprojects.walletok.R;
 public class SplahScreenActivity extends AppCompatActivity {
 
     private SharedPreferences preferences;
+    private int tempo = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splah_screen);
 
-
-
         preferences =  PreferenceManager.getDefaultSharedPreferences(this);
 
+        boolean desativarAbertura = preferences.getBoolean(getString(R.string.desativa_tela_abertura), false);
+        if(desativarAbertura){
+            tempo = 0;
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -36,10 +38,7 @@ public class SplahScreenActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }else{
-                    //TODO verifica se a preferencia "manter conectado" esta true, caso sim, nao exibir tela de login
-
                     boolean manterAtivo = preferences.getBoolean(getString(R.string.manter_conectado), false);
-                    Log.i("ATIVO", "run: Manter Conectado " + manterAtivo);
                     if(manterAtivo){
                         startActivity(new Intent(SplahScreenActivity.this, MainActivity.class));
                         finish();
@@ -52,6 +51,6 @@ public class SplahScreenActivity extends AppCompatActivity {
                 }
 
             }
-        }, 1500);
+        }, tempo);
     }
 }
