aac13c496bfb68213d142bfaa2ebada670943aa6
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-11-13 21:50:31 -0200

    Layout inicial do cadastro de Cartao Credito

---
+++ app/src/main/java/com/asuprojects/walletok/ui/CartaoCreditoActivity.java
@@ -0,0 +1,15 @@
+package com.asuprojects.walletok.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asuprojects.walletok.R;
+
+public class CartaoCreditoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartao_credito);
+    }
+}
